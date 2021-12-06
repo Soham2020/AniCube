2e2d7ab8b4b7922c30f3faa8498ebfafbb727f0a
Soham Das <majumder70.ranu@gmail.com>
2021-12-06 11:04:35 +0530

    Resized according to window

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
@@ -22,3 +22,10 @@ function animate() {
     renderer.render(scene, camera);
 }
 animate();
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize, false);
